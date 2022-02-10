ac642d059d77a0d6f0c16fa87ca2e4100fbc9ecc
Sameerun1 <sameerun.shaikh@gmail.com>
2022-02-10 22:26:00 +0100

    My resume in HTML,CSS and JS

---
+++ data/profile.js
@@ -0,0 +1,125 @@
+const profileData = {
+    title: 'Resume',
+    name: 'Sameerun Acchukatla',
+    sub_title: 'Java Developer',
+    logoURL: 'assets/images/dp.jpg',
+    about: {
+        intro: `Passionate and dedicated learner looking to restart the career in Software Development as a <strong>Junior Java Developer and Tester.</strong>
+     and it's applications.`,
+        contact: {
+            email: 'sameerun.shaikh@gmail.com',
+            phone: '+32 473514873',
+            address: 'Belgium, Brussels'
+        }
+    },
+    links: [
+        { title: 'Codepen', src: 'https://codepen.io/sameerun' },
+        { title: 'Github', src: 'https://github.com/sameerun-acchukatla' },
+        { title: 'LinkedIn', src: 'https://www.linkedin.com/in/sameerun-acchukatla-a33869196' }
+    ],
+    education: [
+
+        {
+            alma: 'AP,India',
+            duration: '2005 - 2008',
+            std: 'Master in Computer Application',
+            score:'70.00%'
+        },
+        {
+            alma: 'AP,India',
+            duration: '2003-2005',
+            std: 'Bachelor in Computer Science',
+            score:'65.68%'
+        }
+
+    ],
+    skills: [
+        {
+            category: 'Programming Languages',
+            topics: [
+                'Core Java','Object-Oriented Programming(OOP)', 'C', 'C++','HTML5','CSS',
+                'JavaScript'
+
+            ]
+        },
+        {
+            category: 'Frameworks / Libraries',
+            topics: [
+                'Spring',
+                'Spring Boot',
+                'Maven',
+                'JUnit'
+            ]
+        },
+        {
+            category: 'Databases',
+            topics: ['SQL','JDBC']
+        }
+    ],
+    experiences: [
+        {
+            organization: 'Lara Technologies pvt Ltd,Bangalore,India',
+            title: `Junior Java Developer`,
+            date: 'May 2009- Nov 2011'
+        }
+    ],
+    certifications: [
+        {
+            desc: `<strong>Programming Foundations with JavaScript, HTML and CSS</strong> by <strong>Coursera</strong>.
+      (<a target='_blank' rel='noreferrer' href='https://www.coursera.org/account/accomplishments/verify/H5U5GVGER5SY'>
+      Coursera</a>)`,
+            date: ''
+
+        },
+        {
+            desc: `<strong>Java Programming: Solving Problems with Software</strong> by <strong>Coursera</strong>.
+      (<a target='_blank' rel='noreferrer' href='https://www.coursera.org/account/accomplishments/verify/YDWFSTGLT2D4'>
+      Coursera</a>)`,
+            date: ''
+
+        },
+        {
+            desc: `<strong>Java Programming: Build a Recommendation System</strong> by <strong>Coursera</strong>.
+      (<a target='_blank' rel='noreferrer' href='https://www.coursera.org/account/accomplishments/verify/QE4J3PAGQC4V'>
+      Coursera</a>)`,
+            date: ''
+
+        },
+        {
+            desc: `<strong>Java Programming: Principles of Software Design</strong> by <strong>Coursera</strong>.
+      (<a target='_blank' rel='noreferrer' href='https://www.coursera.org/account/accomplishments/verify/NAFXB5PWLYZB'>
+      Coursera</a>)`,
+            date: ' '
+
+        },
+        {
+            desc: `<strong>Java Programming: Arrays, Lists, and Structured Data</strong> by <strong>Coursera</strong>.
+      (<a target='_blank' rel='noreferrer' href='https://www.coursera.org/account/accomplishments/verify/XRZWVNHCW2PN('>
+      Coursera</a>)`,
+            date: ''
+
+        },
+        {
+            desc: '<strong>Java EE Developer - Training</strong> by <strong>INTEC</strong>',
+            date: ''
+
+        }
+
+    ],
+    languages: [
+
+        {
+            title: 'English',
+            proficiency:'Very Good'
+        },
+        {
+            title: 'Dutch',
+            proficiency:'Good-VDAB- Module 2.4'
+        },
+        {
+            title: 'French',
+            proficiency:'Basic'
+        }
+],
+    events: []
+};
